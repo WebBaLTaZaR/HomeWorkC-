7a05a7550c6e74a867cd7f8ce79ce9d1dca25f77
BaLTaZaR <gricaev_sergey@mail.ru>
2023-03-26 00:20:09 +0600

    выполнил 8 задание

---
+++ Task8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+for(int i = 2;i < number; i++ ){
+	if (!Convert.ToBoolean(i % 2)){
+		Console.WriteLine(i);
+	}
+}
