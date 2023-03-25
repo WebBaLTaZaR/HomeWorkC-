35437729186391a0ca66317ee97ec7023431b79e
BaLTaZaR <gricaev_sergey@mail.ru>
2023-03-25 17:03:43 +0600

    выполнил 4 задание, но почему-то работает не так как надо, если самое большое число третье в списке, то его не проверяют, не получается разобраться без дебагера

---
+++ Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.Write("Введите первое число ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Сейчас найдём из них максимальное");
+if (firstNumber > secondNumber){
+	if(firstNumber > thirdNumber){
+		Console.WriteLine($"{firstNumber} явно больше, чем остальные");
+}}else if(secondNumber > firstNumber) {
+	if(secondNumber > thirdNumber) {
+	Console.WriteLine($"{secondNumber} явно больше, чем остальные");
+}}else if(thirdNumber > firstNumber) {
+	if(thirdNumber > secondNumber){
+	Console.WriteLine($"{thirdNumber} явно больше, чем остальные");
+}}else{
+	Console.WriteLine("Однозначно самого большого здесь нет");
+}
+Console.WriteLine("Если хочешь повторить, введи в консоли 'dotnet run' ");
