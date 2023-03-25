68cea0a1e85a5f6710e12027943f13d2ab1a1a2c
BaLTaZaR <gricaev_sergey@mail.ru>
2023-03-25 21:40:44 +0600

    сделал 6 задание

---
+++ Task6/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+
+Console.Write("ВВедите первое число ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("ВВедите второе число ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (Convert.ToBoolean(firstNumber % 2)){
+	Console.WriteLine($"{firstNumber} - нечётное число");
+}else{
+	Console.WriteLine($"{firstNumber} - чётное число");
+}
+
+if (Convert.ToBoolean(secondNumber % 2)){
+	Console.WriteLine($"{secondNumber} - нечётное число");
+}else{
+	Console.WriteLine($"{secondNumber} - чётное число");
+}
