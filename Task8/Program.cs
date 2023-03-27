468795e1765f089c14cd0433fc41bfa0666ebb37
BaLTaZaR <gricaev_sergey@mail.ru>
2023-03-27 17:41:45 +0600

    оптимизировал мелочь

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -3,7 +3,7 @@
 Console.WriteLine("Введите число: ");
 
 int number = Convert.ToInt32(Console.ReadLine());
-for(int i = 2;i < number; i++ ){
+for(int i = 2;i <= number; i++ ){
 	if (!Convert.ToBoolean(i % 2)){
 		Console.WriteLine(i);
 	}
