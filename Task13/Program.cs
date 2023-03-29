d9c947c17c0a20a55a4347980d0664d193ed8480
BaLTaZaR <gricaev_sergey@mail.ru>
2023-03-29 17:21:10 +0600

    пока не знаю, как решить задачу

---
+++ Task13/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number);
+int hundreds = (number % 1000);
+int dozens = (number % 100);
+int units = (number % 10);
+if (number < 100){
+Console.WriteLine("Третьего числа нет");
+}
+else if (number > 999 && number < 10000 ){
+	Console.WriteLine(hundreds);
+}
+// решения ещё не придумал
