164124c4341db576af01d25dd9d05f9ef99dcee0
BaLTaZaR <gricaev_sergey@mail.ru>
2023-03-29 16:50:31 +0600

    помучался, но сделал

---
+++ Task15/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.Write("Введите цифру от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while(number > 7 || number < 1){
+	Console.Write("Введите цифру от 1 до 7: ");
+	number = Convert.ToInt32(Console.ReadLine());
+}
+if(number == 6 || number == 7)
+	Console.WriteLine("Да детка, это выходные!");
+else 
+	Console.WriteLine("Это будний день, иди работать!");
