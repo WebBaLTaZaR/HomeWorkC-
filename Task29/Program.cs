26d61d1587ba5ab1346344159c1738fc635305c1
BaLTaZaR <gricaev_sergey@mail.ru>
2023-04-03 15:24:28 +0600

    решил 29 задачу

---
+++ Task29/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+int Prompt(string message){
+	Console.Write(message);
+	string? value = Console.ReadLine();
+	int result = Convert.ToInt32(value);
+	return result;
+}
+
+int n =Prompt("Введите кол-во элементов массива: ");
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1, 11); // [1, 10]
+Console.WriteLine($"[{string.Join(", ", array)}]");
