e338391dd7cc87f8b3452aba1dafcf19ac6201fb
BaLTaZaR <gricaev_sergey@mail.ru>
2023-04-03 12:52:06 +0600

    решил 27 задачу

---
+++ Task27/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+int Prompt(string message){
+	Console.Write(message);
+	string? value = Console.ReadLine();
+	int result = Convert.ToInt32(value);
+	return result;
+}
+
+int number = Prompt("Введите число: ");
+
+int result = 0;
+while(number > 0){
+	result = result + number%10;
+	number = number/10;
+}
+Console.WriteLine(result);
+    
