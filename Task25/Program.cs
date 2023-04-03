ae7c21b1e32e1d6e1e043cd6b163e3cdecaa1c0e
BaLTaZaR <gricaev_sergey@mail.ru>
2023-04-03 11:50:46 +0600

    решил 25 задачу

---
+++ Task25/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+int Prompt(string message){
+	Console.Write(message);
+	string? value = Console.ReadLine();
+	int result = Convert.ToInt32(value);
+	return result;
+}
+
+int numberFirst = Prompt("Введите первое число : ");
+int numberSecond = Prompt("Введите второе число : ");
+int result = 1;
+for (int i = 1; i <= numberSecond ; i++) {
+	result *= numberFirst;
+}
+Console.WriteLine(result);
