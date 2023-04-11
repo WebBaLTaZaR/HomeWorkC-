3f7b714a60b4d66dcc509f1871a7029c741ea8ff
BaLTaZaR <gricaev_sergey@mail.ru>
2023-04-11 17:31:12 +0600

    решил 41 задачу

---
+++ Task41/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+int Prompt(string message)
+{
+	Console.Write(message);
+	string? value = Console.ReadLine();
+	int result = Convert.ToInt32(value);
+	return result;
+}
+
+int number = Prompt("Введите число: ");
+int[] array = new int[number];
+
+int count = 0;
+for (int i = 0; i < number; i++)
+{
+	array[i] = new Random().Next(-10, 11);
+	if (array[i] > 0)
+	{
+		count++;
+
+	}
+}
+Console.WriteLine($" Массив [{String.Join(",", array)}]");
+Console.WriteLine(count);
