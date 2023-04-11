38365ab185198e7613eec2a80d0883ceafa09453
BaLTaZaR <gricaev_sergey@mail.ru>
2023-04-11 12:12:01 +0600

    решил 40 задачу

---
+++ Task40/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+int Prompt(string message){
+	Console.Write(message);
+	string? value = Console.ReadLine();
+	int result = Convert.ToInt32(value);
+	return result;
+}
+
+int a = Prompt("Введите первую сторону треугольника: ");
+int b = Prompt("Введите вторую сторону треугольника: ");
+int c = Prompt("Введите третью сторону треугольника: ");
+
+if(a <= (b + c) && b <= (a + c) && c <= (b + a) ){
+	Console.WriteLine("yes");
+}
+else Console.WriteLine("no");
