93124248cb765b584306de58b6cdb9417f887af0
BaLTaZaR <gricaev_sergey@mail.ru>
2023-04-13 15:42:24 +0600

    решил 47 задачу

---
+++ Task47/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+
+
+void InputMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = Math.Round(new Random().NextDouble() * (30 - 10) + 10, 2);
+    }
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+double[,] matrix = new double [size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+
+
+
