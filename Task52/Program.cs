cbfddeb71cfc2676c07c98d88f64ae9225b000f4
BaLTaZaR <gricaev_sergey@mail.ru>
2023-04-16 00:12:38 +0600

    решил 52 задачу

---
+++ Task52/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace HelloWorld
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+
+            Console.Clear();
+
+
+            void InputMatrix(int[,] matrix)
+            {
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                        matrix[i, j] = new Random().Next(10, 99);
+                }
+            }
+
+            void PrintMatrix(int[,] matrix)
+            {
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                        Console.Write($"{matrix[i, j]} \t");
+                    Console.WriteLine();
+                }
+            }
+            Console.Write("Введите размеры массива: ");
+            int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+            int[,] matrix = new int[size[0], size[1]];
+            InputMatrix(matrix);
+            Console.WriteLine("Начальный массив");
+            Console.WriteLine();
+            PrintMatrix(matrix);
+            CheckMatrix(matrix);
+
+            void CheckMatrix(int[,] matrix)
+            {
+                
+                double count = Convert.ToDouble(0);
+                double result = Convert.ToDouble(0);
+                double empty = Convert.ToDouble(0);
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                { 
+                    for (int j = 0; j < matrix.GetLength(1); j++){
+                        count += matrix[j, i]; 
+                        result = count;
+                }
+                Console.WriteLine(Math.Round(result /= matrix.GetLength(1),2));
+                count = empty;
+            }}
+        }}}
+
