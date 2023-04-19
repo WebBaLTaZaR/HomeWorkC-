f86d75732f50d9fb8d7b7d52526b1ae33e7ff5a2
BaLTaZaR <gricaev_sergey@mail.ru>
2023-04-19 16:19:38 +0600

    решил 54

---
+++ Task54/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.Clear();
+
+
+void InputMatrix(int[,] matrix)
+{
+	for (int i = 0; i < matrix.GetLength(0); i++)
+	{
+		for (int j = 0; j < matrix.GetLength(1); j++)
+			matrix[i, j] = new Random().Next(10, 20);
+	}
+}
+
+void PrintMatrix(int[,] matrix)
+{
+	for (int i = 0; i < matrix.GetLength(0); i++)
+	{
+		for (int j = 0; j < matrix.GetLength(1); j++)
+			Console.Write($"{matrix[i, j]} \t");
+		Console.WriteLine();
+	}
+}
+
+void MinSortMatrix(int[,] matrix)
+{
+	for (int i = 0; i < matrix.GetLength(0); i++)
+	{
+		for (int j = 0; j < matrix.GetLength(1); j++)
+		{
+			for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+			{
+				if (matrix[i, k] < matrix[i, k + 1])
+				{
+					int count = matrix[i, k + 1];
+					matrix[i, k + 1] = matrix[i, k];
+					matrix[i, k] = count;
+				}
+			}
+		}
+	}
+}
+
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine("Начальный массив");
+Console.WriteLine();
+PrintMatrix(matrix);
+MinSortMatrix(matrix);
+Console.WriteLine();
+PrintMatrix(matrix);
