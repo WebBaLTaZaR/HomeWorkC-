1c386886ed28c5af67706434d6a075e12ca90af0
BaLTaZaR <gricaev_sergey@mail.ru>
2023-04-21 15:14:13 +0600

    сделал 66

---
+++ Task66/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+int f(int n, int m)
+{
+    if (n == m || n == 0)
+        return m;
+    return f(m, n - 1) + m;
+}
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(n, m));
