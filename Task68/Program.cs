d9f4facdf9f52ad59d1fc13e52e58585001dae21
BaLTaZaR <gricaev_sergey@mail.ru>
2023-04-21 15:30:31 +0600

    сделал 68

---
+++ Task68/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+int f(int n, int m)
+{
+	if (n == 0) return m + 1;
+	if (n != 0 && m == 0) return f(n - 1, 1);
+	if (n > 0 && m > 0) return f(n - 1, f(n, m - 1));
+	return f(n, m);
+}
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(n, m));
