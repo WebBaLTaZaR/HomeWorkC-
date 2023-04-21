16d594acefa0ca44b2f40754b01f51af207fb713
BaLTaZaR <gricaev_sergey@mail.ru>
2023-04-21 14:48:43 +0600

    сделал 69

---
+++ Task69/Program.cs
@@ -0,0 +1,14 @@
+﻿int f(int a, int b)
+{
+    if (b == 0)
+        return 1;
+    return f(a,b - 1) * a;
+}
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(a,b));
