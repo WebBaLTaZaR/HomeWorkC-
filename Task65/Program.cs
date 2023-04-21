cfb217bc1cad673c1a00345a923884b03f35a395
BaLTaZaR <gricaev_sergey@mail.ru>
2023-04-21 13:46:30 +0600

    решил 65

---
+++ Task65/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+
+string f(int n)
+{
+    if (n == m)
+        return $"{m} ";
+    return f(n - 1) + $"{n} ";
+}
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(f(n));
