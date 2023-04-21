ee7cd2c3d43c374f241a3cded647eab8c62545b7
BaLTaZaR <gricaev_sergey@mail.ru>
2023-04-21 14:38:34 +0600

    сделал 67

---
+++ Task67/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+
+
+int f(int n)
+{
+    if (n % 10 == 0 || n < 10 || n / 10 == n)
+        return n;
+    return f(n / 10) + n % 10  ;
+}
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(n));
